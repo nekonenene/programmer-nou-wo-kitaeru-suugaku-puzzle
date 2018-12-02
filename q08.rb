b312d77e552ba17635207ae77f4d2584518246d5
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2018-12-02 16:03:44 +0900

    Solving Q08....

---
+++ q08.rb
@@ -0,0 +1,40 @@
+# 同じ場所を通らない掃除ロボットを考えます。このロボットは、前後左右にのみ移動することができます。
+# このロボットが12回移動するとき、考えられる移動経路のパターンが何通りあるかを求めてください。
+
+class Q08
+  MOVE_MAX = 12
+
+  def main
+    pos = { x: 0, y: 0 }
+
+    answer = move(pos)
+    p answer
+    puts answer.count
+  end
+
+  def move(pos, move_history = [], dir_history = [], ans_arr = [])
+    return if move_history.count > MOVE_MAX
+    return if move_history.include?(pos)
+    move_history_new = move_history.clone.push(pos.clone)
+
+    return ans_arr.push(dir_history) if move_history.count == MOVE_MAX
+
+    %w(u d l r).each do |dir|
+      case dir
+      when "u"
+        pos[:y] -= 1
+      when "d"
+        pos[:y] += 1
+      when "l"
+        pos[:x] -= 1
+      when "r"
+        pos[:x] += 1
+      end
+      move(pos, move_history_new, dir_history.clone.push(dir), ans_arr)
+    end
+
+    ans_arr
+  end
+end
+
+Q08.new.main
