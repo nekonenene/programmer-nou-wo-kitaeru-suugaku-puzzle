06994095548cce3799880cf13ddaa0521fe7a9f7
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2018-12-02 23:37:46 +0900

    Improve A.10

--- q10.rb
+++ q10.rb
@@ -6,14 +6,20 @@ class Q10
   EUROPEAN = [0,32,15,19,4,21,2,25,17,34,6,27,13,36,11,30,8,23,10,5,24,16,33,1,20,14,31,9,22,18,29,7,28,12,35,3,26] # 37区分
 
   def main
+    start_time = Time.now
+
     answer = 0
     (2..36).each do |choices|
-      answer += 1 if search_max(EUROPEAN, choices) < search_max(AMERICAN, choices)
+      answer += 1 if search_max2(EUROPEAN, choices) < search_max2(AMERICAN, choices)
     end
 
     puts answer
+
+    end_time = Time.now
+    puts end_time - start_time
   end
 
+  # 0.0038s
   def search_max(roulette_arr, choices, start = 0, max = 0)
     return max if start - 1 == roulette_arr.size - 1
 
@@ -24,6 +30,23 @@ class Q10
     end
     max = amount if amount > max
     search_max(roulette_arr, choices, start + 1, max)
+  end
+
+  # 左端を減らし右端を増やす、という処理にすることで処理速度改善を狙える
+  # 0.00031s : 従来のやり方の約10分の1
+  def search_max2(roulette_arr, choices)
+    # self[start, length] : start 番目から length 個の要素を含む部分配列を返します
+    amount = roulette_arr[0, choices].sum
+    max = amount
+
+    roulette_arr.size.times do |i|
+      amount -= roulette_arr[i]
+      amount += roulette_arr[(i + choices) % roulette_arr.size]
+
+      max = amount if amount > max
+    end
+
+    max
   end
 end
 
